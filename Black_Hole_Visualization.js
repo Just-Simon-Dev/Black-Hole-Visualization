ee731a6258bd2cfaffb3508cbb8222e37e4e7769
Just-Simon-Dev <szymonsoborowski@gmail.com>
2022-04-12 23:03:08 +0200

    update all values on change

--- Black_Hole_Visualization.js
+++ Black_Hole_Visualization.js
@@ -2,10 +2,11 @@ const c = 30;
 const G = 1;
 const dt = 0.1;
 
-
-let numObj = 10
 let type = "Planet"
+let numObj = 10
 let BlackHoleMass = 100
+let objVel = 20
+let objMass = 10
 
 
 let blackhole;
@@ -18,19 +19,23 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(RADIUS);
   
+  document.getElementById("type").addEventListener("change", () => {window.location.reload();})
   document.getElementById("ObjectNumber").addEventListener("change", () => {window.location.reload();})
   document.getElementById("Mass").addEventListener("change", () => {window.location.reload();})
+  document.getElementById("ObjectMass").addEventListener("change", () => {window.location.reload();})
+  document.getElementById("ObjectVel").addEventListener("change", () => {window.location.reload();})
 
-
+  type = document.getElementById("type").value
   numObj = document.getElementById("ObjectNumber").value
   BlackHoleMass = document.getElementById("Mass").value
-  console.log(numObj)
+  objVel = document.getElementById("ObjectVel").value
+  objMass = document.getElementById("ObjectMass").value
 
   blackhole = new BlackHole(width/2, height/2, BlackHoleMass);
   let start = height/2;
   let end = height/2 - blackhole.rs*2.6;
   for(let y = 0; y < numObj; y++){
-     objects.push(new SpaceObject(width-20, y*(height/numObj/2), type, 10, type == "Photon" ? c : 20));
+     objects.push(new SpaceObject(width-20, y*(height/numObj/2), type, objMass, type == "Photon" ? c : objVel));
   }
 }
 
